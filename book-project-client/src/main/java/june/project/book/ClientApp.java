1565cebeca5d4f3e597cc1d8682cdf2bdab452d4
JUNE-29 <jiyuni529@gmail.com>
2020-07-13 23:11:09 +0900

    [add] v27_1버전 추가 및 폴더 생성

---
+++ book-project-client/src/main/java/june/project/book/ClientApp.java
@@ -0,0 +1,7 @@
+package june.project.book;
+
+public class ClientApp {
+  public static void main(String[] args) {
+    System.out.println("클라이언트 책 관리 시스템입니다.");
+  }
+}
---
+++ book-project-server/src/main/java/june/project/book/ServerApp.java
@@ -0,0 +1,8 @@
+package june.project.book;
+
+public class ServerApp {
+
+  public static void main(String[] args) {
+    System.out.println("서버 책 관리 시스템입니다");
+  }
+}
