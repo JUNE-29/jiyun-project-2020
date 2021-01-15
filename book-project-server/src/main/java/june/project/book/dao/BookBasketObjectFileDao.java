fdda6dfe959ebd5eef2c24bb5601533985ce50f6
JUNE-29 <jiyuni529@gmail.com>
2021-01-15 12:58:06 +0900

    [add] v027_8 버전 추가

--- book-project-server/src/main/java/june/project/book/dao/BookBasketObjectFileDao.java
+++ book-project-server/src/main/java/june/project/book/dao/BookBasketObjectFileDao.java
@@ -1,57 +1,15 @@
 package june.project.book.dao;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.util.ArrayList;
 import java.util.List;
 import june.project.book.domain.BookBasket;
 
-public class BookBasketObjectFileDao {
+public class BookBasketObjectFileDao extends AbstractObjectFileDao<BookBasket> {
 
-  String filename;
-  List<BookBasket> list;
 
   public BookBasketObjectFileDao(String filename) {
-    this.filename = filename;
-    list = new ArrayList<>();
-    loadData();
+    super(filename);
   }
 
-  @SuppressWarnings("unchecked")
-  private void loadData() {
-    File file = new File(filename);
-
-    try (ObjectInputStream in =
-        new ObjectInputStream(new BufferedInputStream(new FileInputStream(file)))) {
-
-      list = (List<BookBasket>) in.readObject();
-
-      System.out.printf("총 %d 개의 즐겨찾는 도서의 데이터를 로딩했습니다.\n", list.size());
-    } catch (Exception e) {
-      System.out.println("파일 읽기 중 오류 발생! -" + e.getMessage());
-    }
-  }
-
-  private void saveData() {
-    File file = new File(filename);
-
-    try (ObjectOutputStream out =
-        new ObjectOutputStream(new BufferedOutputStream(new FileOutputStream(file)))) {
-
-      out.reset();
-      out.writeObject(list);
-
-      System.out.printf("총 %d 개의 즐겨찾는 도서의 데이터를 저장했습니다.\n", list.size());
-    } catch (IOException e) {
-      System.out.println("파일 쓰기 중 오류 발생! - " + e.getMessage());
-    }
-  }
 
   public List<BookBasket> findAll() throws Exception {
     return list;
@@ -102,14 +60,14 @@ public class BookBasketObjectFileDao {
     }
 
     list.remove(index);
-    saveData();
     return 1;
   }
 
 
-  private int indexOf(int no) {
+  @Override
+  protected <K> int indexOf(K key) {
     for (int i = 0; i < list.size(); i++) {
-      if (list.get(i).getNo() == no) {
+      if (list.get(i).getNo() == (int) key) {
         return i;
       }
     }
--- book-project-server/src/main/java/june/project/book/dao/BookBoardObjectFileDao.java
+++ book-project-server/src/main/java/june/project/book/dao/BookBoardObjectFileDao.java
@@ -1,57 +1,13 @@
 package june.project.book.dao;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.util.ArrayList;
 import java.util.List;
 import june.project.book.domain.BookBoard;
 
-public class BookBoardObjectFileDao {
+public class BookBoardObjectFileDao extends AbstractObjectFileDao<BookBoard> {
 
-  String filename;
-  List<BookBoard> list;
 
   public BookBoardObjectFileDao(String filename) {
-    this.filename = filename;
-    list = new ArrayList<>();
-    loadData();
-  }
-
-  @SuppressWarnings("unchecked")
-  private void loadData() {
-    File file = new File(filename);
-
-    try (ObjectInputStream in =
-        new ObjectInputStream(new BufferedInputStream(new FileInputStream(file)))) {
-
-      list = (List<BookBoard>) in.readObject();
-
-      System.out.printf("총 %d 개의 도서 데이터를 로딩했습니다.\n", list.size());
-
-    } catch (Exception e) {
-      System.out.println("파일 읽기 중 오류 발생! - " + e.getMessage());
-    }
-  }
-
-  private void saveData() {
-    File file = new File(filename);
-
-    try (ObjectOutputStream out =
-        new ObjectOutputStream(new BufferedOutputStream(new FileOutputStream(file)))) {
-
-      out.reset(); // 기존의 직렬화 수행 중에 캐시된(임시 보관된) 데이터 초기화
-      out.writeObject(list);
-
-      System.out.printf("총 %d 개의 도서 데이터를 저장했습니다.\n", list.size());
-    } catch (IOException e) {
-      System.out.println("파일 쓰기 중 오류 발생! - " + e.getMessage());
-    }
+    super(filename);
   }
 
   // 서블릿 객체들이 데이터를 다룰 때 사용할 메서드를 정의한다.
@@ -102,9 +58,11 @@ public class BookBoardObjectFileDao {
     return 1;
   }
 
-  private int indexOf(int no) {
+
+  @Override
+  protected <K> int indexOf(K key) {
     for (int i = 0; i < list.size(); i++) {
-      if (list.get(i).getNo() == no) {
+      if (list.get(i).getNo() == (int) key) {
         return i;
       }
     }
