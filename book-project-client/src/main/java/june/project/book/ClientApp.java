bf0e3d436f1a1e28c55cfd9b3c5c164a59f1140e
JUNE-29 <jiyuni529@gmail.com>
2021-01-21 22:40:55 +0900

    [add] v028_1 버전 추가 - Stateless 방식

--- book-project-client/src/main/java/june/project/book/ClientApp.java
+++ book-project-client/src/main/java/june/project/book/ClientApp.java
@@ -42,15 +42,24 @@ public class ClientApp {
   Scanner keyboard = new Scanner(System.in);
   Prompt prompt = new Prompt(keyboard);
 
+  Deque<String> commandStack;
+  Queue<String> commandQueue;
+
+  String host;
+  int port;
+
+  public ClientApp() {
+
+    commandStack = new ArrayDeque<>();
+    commandQueue = new LinkedList<>();
+
+  }
+
   public void service() {
 
-    String serverAddr = null;
-    int port = 0;
-
     try {
-
       // 사용자로부터 접속할 서버의 주소와 포트 번호를 입력 받는다.
-      serverAddr = prompt.inputString("서버? ");
+      host = prompt.inputString("서버? ");
       port = prompt.inputInt("포트? ");
 
     } catch (Exception e) {
@@ -60,9 +69,40 @@ public class ClientApp {
     }
 
 
-    try (
-        // 서버와 연결
-        Socket socket = new Socket(serverAddr, port);
+    while (true) {
+      String command;
+      command = prompt.inputString("\n명령> ");
+
+      if (command.length() == 0)
+        continue;
+
+      if (command.equals("history")) {
+        printCommandHistory(commandStack.iterator());
+        continue;
+
+      } else if (command.equals("history2")) {
+        printCommandHistory(commandQueue.iterator());
+        continue;
+
+      } else if (command.equals("quit")) {
+        break;
+      }
+
+      // 명령어를 받아서 넘긴다.
+      commandStack.push(command);
+      commandQueue.offer(command);
+
+      processCommand(command);
+
+    }
+
+    keyboard.close();
+
+  }
+
+  private void processCommand(String command) {
+
+    try (Socket socket = new Socket(host, port);
 
         // 소켓을 통해 데이터를 읽고 쓰는 도구를 준비한다.
         ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
@@ -70,99 +110,67 @@ public class ClientApp {
 
       System.out.println("서버와 연결되었음!");
 
-      processCommand(out, in);
+      // Dao 프록시 객체 준비
+      BookBoardDaoProxy bookBoardDao = new BookBoardDaoProxy(in, out);
+      BookBasketDaoProxy bookBasketDao = new BookBasketDaoProxy(in, out);
+      BookmarkDaoProxy bookmarkDao = new BookmarkDaoProxy(in, out);
+      MemberDaoProxy memberDao = new MemberDaoProxy(in, out);
 
-      System.out.println("서버와 연결을 끊었음!");
+      HashMap<String, Command> commandMap = new HashMap<>();
 
-    } catch (Exception e) {
-      System.out.println("예외 발생");
-      e.printStackTrace();
-    }
-    keyboard.close();
+      commandMap.put("/basket/add", new BookBasketAddCommand(bookBasketDao, prompt));
+      commandMap.put("/basket/list", new BookBasketListCommand(bookBasketDao));
+      commandMap.put("/basket/detail", new BookBasketDetailCommand(bookBasketDao, prompt));
+      commandMap.put("/basket/update", new BookBasketUpdateCommand(bookBasketDao, prompt));
+      commandMap.put("/basket/delete", new BookBasketDeleteCommand(bookBasketDao, prompt));
 
-  }
+      commandMap.put("/bookmark/add", new BookmarkAddCommand(bookmarkDao, prompt));
+      commandMap.put("/bookmark/list", new BookmarkListCommand(bookmarkDao));
+      commandMap.put("/bookmark/detail", new BookmarkDetailCommand(bookmarkDao, prompt));
+      commandMap.put("/bookmark/update", new BookmarkUpdateCommand(bookmarkDao, prompt));
+      commandMap.put("/bookmark/delete", new BookmarkDeleteCommand(bookmarkDao, prompt));
 
-  private void processCommand(ObjectOutputStream out, ObjectInputStream in) {
+      commandMap.put("/book/add", new BookBoardAddCommand(bookBoardDao, prompt));
+      commandMap.put("/book/list", new BookBoardListCommand(bookBoardDao));
+      commandMap.put("/book/detail", new BookBoardDetailCommand(bookBoardDao, prompt));
+      commandMap.put("/book/update", new BookBoardUpdateCommand(bookBoardDao, prompt));
+      commandMap.put("/book/delete", new BookBoardDeleteCommand(bookBoardDao, prompt));
 
-    Deque<String> commandStack = new ArrayDeque<>();
-    Queue<String> commandQueue = new LinkedList<>();
+      commandMap.put("/member/add", new MemberAddCommand(memberDao, prompt));
+      commandMap.put("/member/list", new MemberListCommand(memberDao));
+      commandMap.put("/member/detail", new MemberDetailCommand(memberDao, prompt));
+      commandMap.put("/member/update", new MemberUpdateCommand(memberDao, prompt));
+      commandMap.put("/member/delete", new MemberDeleteCommand(memberDao, prompt));
 
-    // Dao 프록시 객체 준비
-    BookBoardDaoProxy bookBoardDao = new BookBoardDaoProxy(in, out);
-    BookBasketDaoProxy bookBasketDao = new BookBasketDaoProxy(in, out);
-    BookmarkDaoProxy bookmarkDao = new BookmarkDaoProxy(in, out);
-    MemberDaoProxy memberDao = new MemberDaoProxy(in, out);
-
-    HashMap<String, Command> commandMap = new HashMap<>();
-
-    commandMap.put("/basket/add", new BookBasketAddCommand(bookBasketDao, prompt));
-    commandMap.put("/basket/list", new BookBasketListCommand(bookBasketDao));
-    commandMap.put("/basket/detail", new BookBasketDetailCommand(bookBasketDao, prompt));
-    commandMap.put("/basket/update", new BookBasketUpdateCommand(bookBasketDao, prompt));
-    commandMap.put("/basket/delete", new BookBasketDeleteCommand(bookBasketDao, prompt));
-
-    commandMap.put("/bookmark/add", new BookmarkAddCommand(bookmarkDao, prompt));
-    commandMap.put("/bookmark/list", new BookmarkListCommand(bookmarkDao));
-    commandMap.put("/bookmark/detail", new BookmarkDetailCommand(bookmarkDao, prompt));
-    commandMap.put("/bookmark/update", new BookmarkUpdateCommand(bookmarkDao, prompt));
-    commandMap.put("/bookmark/delete", new BookmarkDeleteCommand(bookmarkDao, prompt));
-
-    commandMap.put("/book/add", new BookBoardAddCommand(bookBoardDao, prompt));
-    commandMap.put("/book/list", new BookBoardListCommand(bookBoardDao));
-    commandMap.put("/book/detail", new BookBoardDetailCommand(bookBoardDao, prompt));
-    commandMap.put("/book/update", new BookBoardUpdateCommand(bookBoardDao, prompt));
-    commandMap.put("/book/delete", new BookBoardDeleteCommand(bookBoardDao, prompt));
-
-    commandMap.put("/member/add", new MemberAddCommand(memberDao, prompt));
-    commandMap.put("/member/list", new MemberListCommand(memberDao));
-    commandMap.put("/member/detail", new MemberDetailCommand(memberDao, prompt));
-    commandMap.put("/member/update", new MemberUpdateCommand(memberDao, prompt));
-    commandMap.put("/member/delete", new MemberDeleteCommand(memberDao, prompt));
-
-    try {
-      while (true) {
-        String command;
-        command = prompt.inputString("\n명령> ");
-
-        if (command.length() == 0)
-          continue;
-
-        if (command.equals("quit") || command.equals("/server/stop")) {
+      commandMap.put("/server/stop", () -> {
+        try {
           out.writeUTF(command);
           out.flush();
           System.out.println("서버: " + in.readUTF());
           System.out.println("안녕!");
-          break;
 
-        } else if (command.equals("history")) {
-          printCommandHistory(commandStack.iterator());
-          continue;
-
-        } else if (command.equals("history2")) {
-          printCommandHistory(commandQueue.iterator());
-          continue;
+        } catch (Exception e) {
         }
+      });
 
-        commandStack.push(command);
-        commandQueue.offer(command);
+      Command commandHandler = commandMap.get(command);
 
-        Command commandHandler = commandMap.get(command);
-
-        if (commandHandler != null) {
-          try {
-            commandHandler.execute();
-          } catch (Exception e) {
-            System.out.printf("명령어 실행중 오류 발생: %s \n", e.getMessage());
-          }
-        } else {
-          System.out.println("실행할 수 없는 명령입니다.");
-        }
+      if (commandHandler == null) {
+        System.out.println("실행할 수 없는 명령입니다.");
+        return;
       }
+
+      commandHandler.execute();
+
     } catch (Exception e) {
-      System.out.println("프로그램 실행 중 오류 발생!");
+      System.out.printf("명령어 실행중 오류 발생: %s \n", e.getMessage());
+      e.printStackTrace();
     }
-    keyboard.close();
+
+    System.out.println("서버와 연결을 끊었음!");
   }
+
+
 
   private void printCommandHistory(Iterator<String> iterator) {
     int count = 0;
