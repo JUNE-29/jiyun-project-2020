ff061eb7f1c4488f4b2cd978efba5829be5914f2
JUNE-29 <jiyuni529@gmail.com>
2021-03-09 10:21:12 +0900

    [add] v044_1 버전 - 작업1,2 완료

--- book-project-server/src/main/java/june/project/book/AppConfig.java
+++ book-project-server/src/main/java/june/project/book/AppConfig.java
@@ -1,16 +1,15 @@
 package june.project.book;
 
 import java.io.InputStream;
+import javax.sql.DataSource;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import june.project.book.dao.BookBoardDao;
-import june.project.book.dao.BookmarkDao;
-import june.project.book.dao.MemberDao;
-import june.project.book.dao.PhotoBoardDao;
-import june.project.book.dao.PhotoFileDao;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import june.project.sql.MybatisDaoFactory;
 import june.project.sql.PlatformTransactionManager;
 import june.project.sql.SqlSessionFactoryProxy;
@@ -20,16 +19,38 @@ import june.project.sql.SqlSessionFactoryProxy;
 // @Component 애노테이션이 붙은 클래스를 찾아 객체를 생성한다.
 //
 @ComponentScan(value = "june.project.book")
+
+// Spring IoC 컨테이너에서 사용할 Properties 파일을 로딩한다.
+@PropertySource("classpath:june/project/book/conf/jdbc.properties")
+
 public class AppConfig {
 
+  // @PropertySource로 로딩한 .properties 파일의 값을 사용하고 싶다면
+  // 다음 애노테이션을 인스턴스 필드 앞에 붙여야 한다.
+  // Spring IoC 컨테이너가 이 클래스의 객체를 생성할 때
+  // 해당 필드에 프로퍼티 값을 자동으로 주입할 것이다.
 
-  public AppConfig() throws Exception {}
+  @Value("${jdbc.driver}")
+  String jdbcDriver;
 
-  // Spring IoC 컨테이너에 수동으로 객체를 등록하고 싶다면,
-  // 그 객체를 만들어 주는 팩토리 메서드를 정의해야 한다.
-  // => 단 메서드 선언부에 @Bean 애노테이션을 붙여야 한다.
-  // => 그래야만 Spring IoC 컨테이너는
-  // 이 메서드를 호출하고 그 리턴 값을 보관한다.
+  @Value("${jdbc.url}")
+  String jdbcUrl;
+
+  @Value("${jdbc.username}")
+  String jdbcUsername;
+
+  @Value("${jdbc.password}")
+  String jdbcPassword;
+
+  @Bean
+  public DataSource dataSource() {
+    DriverManagerDataSource ds = new DriverManagerDataSource();
+    ds.setDriverClassName(jdbcDriver);
+    ds.setUrl(jdbcUrl);
+    ds.setUsername(jdbcUsername);
+    ds.setPassword(jdbcPassword);
+    return ds;
+  }
 
   @Bean
   public SqlSessionFactory sqlSessionFactory() throws Exception {
@@ -57,40 +78,5 @@ public class AppConfig {
     // 필요한 값이 있다면 이렇게 파라미터로 선언만 하라
     // 단 IoC 컨테이너에 들어 있는 값이어야 한다.
     return new PlatformTransactionManager(sqlSessionFactory);
-  }
-
-  @Bean
-  public BookBoardDao bookBoardDao(MybatisDaoFactory daoFactory) {
-    // 필요한 값이 있다면 이렇게 파라미터로 선언만 하라
-    // 단 IoC 컨테이너에 들어 있는 값이어야 한다.
-    return daoFactory.createDao(BookBoardDao.class);
-  }
-
-  @Bean
-  public BookmarkDao bookmarkDao(MybatisDaoFactory daoFactory) {
-    // 필요한 값이 있다면 이렇게 파라미터로 선언만 하라
-    // 단 IoC 컨테이너에 들어 있는 값이어야 한다.
-    return daoFactory.createDao(BookmarkDao.class);
-  }
-
-  @Bean
-  public MemberDao memberDao(MybatisDaoFactory daoFactory) {
-    // 필요한 값이 있다면 이렇게 파라미터로 선언만 하라
-    // 단 IoC 컨테이너에 들어 있는 값이어야 한다.
-    return daoFactory.createDao(MemberDao.class);
-  }
-
-  @Bean
-  public PhotoBoardDao photoBoardDao(MybatisDaoFactory daoFactory) {
-    // 필요한 값이 있다면 이렇게 파라미터로 선언만 하라
-    // 단 IoC 컨테이너에 들어 있는 값이어야 한다.
-    return daoFactory.createDao(PhotoBoardDao.class);
-  }
-
-  @Bean
-  public PhotoFileDao photoFileDao(MybatisDaoFactory daoFactory) {
-    // 필요한 값이 있다면 이렇게 파라미터로 선언만 하라
-    // 단 IoC 컨테이너에 들어 있는 값이어야 한다.
-    return daoFactory.createDao(PhotoFileDao.class);
   }
 }
