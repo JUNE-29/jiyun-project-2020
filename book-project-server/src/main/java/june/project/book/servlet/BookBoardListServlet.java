a4246380fd08803c429917584ae1b28cfc3dd5a1
JUNE-29 <jiyuni529@gmail.com>
2021-03-19 12:06:31 +0900

    [mod] v047_2 버전 - 작업1 완료

--- book-project-server/src/main/java/june/project/book/servlet/BookBoardListServlet.java
+++ book-project-server/src/main/java/june/project/book/servlet/BookBoardListServlet.java
@@ -20,10 +20,39 @@ public class BookBoardListServlet {
   @RequestMapping("/book/list")
   public void service(Scanner in, PrintStream out) throws Exception {
 
+    out.println("<!DOCTYPE html>");
+    out.println("<html>");
+    out.println("<head>");
+    out.println("  <meta charset='UTF-8'>");
+    out.println("  <title>Book 게시글 목록 </title>");
+    out.println("</head>");
+    out.println("<body>");
+    out.println("  <h1> 게시글 </h1>");
+    out.println("  <a href='/book/addForm'> 새 글</a><br>");
+    out.println("  <table border='1'>");
+    out.println("  <tr>");
+    out.println("    <th>번호</th>");
+    out.println("    <th>도서명</th>");
+    out.println("    <th>책에 대한 점수</th>");
+    out.println("    <th>등록일</th>");
+    out.println("    <th>상태</th>");
+    out.println("  </tr>");
+
     List<BookBoard> bookBoard = bookBoardService.list();
     for (BookBoard book : bookBoard) {
-      out.printf("%d, %s, %d점, %s, %d\n", //
+      out.printf("  <tr>" //
+          + "<td>%d</td>" //
+          + "<td>%s</td>" //
+          + "<td>%d</td>" //
+          + "<td>%s</td>" //
+          + "<td>%d</td>" //
+          + "</tr>\n", //
           book.getNo(), book.getBookTitle(), book.getScore(), book.getDate(), book.getBookStatus());
     }
+
+    out.println("</table>");
+
+    out.println("</body>");
+    out.println("</html>");
   }
 }
